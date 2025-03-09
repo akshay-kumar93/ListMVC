4b26201b954b72be29876095a1c0f3a97076f856
Akshay Kumar <akshaylalti@outlook.com>
2025-03-09 11:07:30 +0530

    first commit

---
+++ Models/ViewDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class ViewDataModel
+{
+    public int AppId { get; set; }
+    public string ParameterName { get; set; }
+    public decimal SellPrice { get; set; }
+    public decimal BuyPrice { get; set; }
+    public bool IsEnable { get; set; }
+    public DateTime InsertedDate { get; set; } = DateTime.Now;
+    public DateTime? UpdatedDate { get; set; }
+}
